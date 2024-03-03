415b4c2f67222e972dbd34ec9b389583426ed858
Eugene <josumkin@yahoo.com>
2024-03-03 14:45:36 +0400

    feat: base queries and mutations added

--- src/routes/graphql/mutation.ts
+++ src/routes/graphql/mutation.ts
@@ -1,8 +1,49 @@
-import { GraphQLObjectType } from "graphql";
+import { PrismaClient } from "@prisma/client";
+import { GraphQLNonNull, GraphQLObjectType, GraphQLString } from "graphql";
+import { newPostInputType, newProfileInputType, newUserInputType, postType, profileType, userType } from "./types/types.js";
 
 
 
 export const rootMutations = new GraphQLObjectType({
     name: 'Mutation',
-    fields: {}
+    fields: () => ({
+
+        createUser: {
+            type: userType,
+            args: {
+                dto: {
+                    type: new GraphQLNonNull(newUserInputType)
+                }
+            },
+            resolve: async (parent, {dto}, context: { prisma: PrismaClient }) => {
+                return await context.prisma.user.create({ data: dto })
+            }
+          },
+
+          createPost: {
+            type: postType,
+            args: {
+                dto: {
+                    type: new GraphQLNonNull(newPostInputType)
+                }
+            },
+            resolve: async (parent, {dto}, context: { prisma: PrismaClient }) => {
+                return await context.prisma.post.create({ data: dto })
+            }
+          },
+
+          createProfile: {
+            type: profileType,
+            args: {
+                dto: {
+                    type: new GraphQLNonNull(newProfileInputType)
+                }
+            },
+            resolve: async (parent, {dto}, context: { prisma: PrismaClient }) => {
+                return await context.prisma.profile.create({
+                    data: dto
+                })
+            }
+          },
+    })
   });
---
+++ src/routes/graphql/types/types.ts
@@ -0,0 +1,181 @@
+import { PrismaClient } from "@prisma/client";
+import { UUIDType } from "./uuid.js";
+import {
+    GraphQLBoolean,
+    GraphQLEnumType,
+    GraphQLFloat,
+    GraphQLInputObjectType,
+    GraphQLInt,
+    GraphQLList,
+    GraphQLNonNull,
+    GraphQLObjectType,
+    GraphQLString
+} from "graphql";
+
+
+
+export const newMemberTypeId = new GraphQLEnumType({
+    name: 'MemberTypeId',
+    values: {
+        basic: {
+            value: 'basic',
+        },
+        business: {
+            value: 'business',
+        }
+    }
+});
+
+export const newMemberType = new GraphQLObjectType({
+    name: 'memberType',
+    fields: () => ({
+        id: {type: newMemberTypeId},
+        discount: {type: GraphQLFloat},
+        postsLimitPerMonth: { type: GraphQLInt },
+    })
+});
+
+
+export const profileType = new GraphQLObjectType({
+    name: 'Profile',
+    fields: () => ({
+        id: {
+            type: new GraphQLNonNull(UUIDType)
+        },
+        isMale: {
+            type: new GraphQLNonNull(GraphQLBoolean)
+        },
+        yearOfBirth: {
+            type: new GraphQLNonNull(GraphQLInt)
+        },
+        userId: {
+            type: new GraphQLNonNull(UUIDType)
+        },
+        memberTypeId: {
+            type: new GraphQLNonNull(newMemberTypeId)
+        },
+        memberType: {
+            type: newMemberTypeId,
+            resolve: async (parent: { memberTypeId: string }, args, context: { prisma: PrismaClient }) => {
+                return await context.prisma.memberType.findUnique({
+                    where: {
+                        id: parent.memberTypeId,
+                    },
+                });
+            }
+        },
+    }),
+});
+
+export const newProfileInputType = new GraphQLInputObjectType({
+    name: 'CreateProfileInput',
+    fields: () => ({
+        isMale: {
+            type: new GraphQLNonNull(GraphQLBoolean)
+        },
+        yearOfBirth: {
+            type: new GraphQLNonNull(GraphQLInt)
+        },
+        userId: {
+            type: new GraphQLNonNull(UUIDType)
+        },
+        memberTypeId: {
+            type: new GraphQLNonNull(newMemberTypeId)
+        }
+    })
+});
+
+export const postType = new GraphQLObjectType({
+    name: 'Post',
+    fields: () => ({
+        id: {
+            type: new GraphQLNonNull(UUIDType)
+        },
+        title: {
+            type: new GraphQLNonNull(GraphQLString)
+        },
+        content: {
+            type: new GraphQLNonNull(GraphQLString)
+        },
+        authorId: {
+            type: new GraphQLNonNull(UUIDType)
+        },
+    })
+})
+
+export const userType = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: {
+            type: UUIDType
+        },
+        name: {
+            type: GraphQLString
+        },
+        balance: {
+            type: GraphQLFloat
+        },
+        profile: {
+            type: profileType,
+            resolve: async (parent: {id: string}, args, context: { prisma: PrismaClient }) => {
+                await context.prisma.profile.findUnique({
+                    where: {
+                        userId: parent.id
+                    }
+                })
+            }
+        },
+        posts: {
+            type: new GraphQLList(postType),
+            resolve: async (parent: {id: string}, args, context: { prisma: PrismaClient }) => {
+                await context.prisma.post.findMany({
+                    where: {
+                        authorId: parent.id
+                    }
+                })
+            }
+        },/*
+        userSubscribedTo: {
+            type: new GraphQLList(userType),
+            resolve: async (parent: {id: string}, args, context: { prisma: PrismaClient }) => {
+                await context.prisma.user.findMany({
+                    where: {
+                        subscribedToUser: {
+                            some: {
+                                subscriberId: parent.id,
+                            }
+                        }
+                    }
+                })
+            }
+        },*/
+        //subscribedToUser: {}
+    }
+});
+
+export const newUserInputType = new GraphQLInputObjectType({
+    name: 'CreateUserInput',
+    fields: () => ({
+        name: {
+            type: new GraphQLNonNull(GraphQLString)
+        },
+        balance: {
+            type: new GraphQLNonNull(GraphQLFloat)
+        }
+    })
+});
+
+export const newPostInputType = new GraphQLInputObjectType({
+    name: 'CreatePostInput',
+    fields: () => ({
+        title: {
+            type: new GraphQLNonNull(GraphQLString)
+        },
+        content: {
+            type: new GraphQLNonNull(GraphQLFloat)
+        },
+        authorId: {
+            type: new GraphQLNonNull(UUIDType)
+        }
+    })
+});
